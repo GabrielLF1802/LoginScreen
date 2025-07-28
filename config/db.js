3cf598eb7ad0cee30d1550ebec546fb6b1bfafd9
Gabriel <gabriellourencofaria@gmail.com>
2025-07-28 19:17:09 -0300

    Estilização do cadastro com bootstrap

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV=='production'){
-    module.exports={mongoURI:'mongodb+srv://gabriel:Jklx18xx!@cluster0.0sgx6iw.mongodb.net/'}
+    module.exports={mongoURI:'link banco de dados'}
 }else{
     module.exports={mongoURI:'mongodb://localhost/login'}
 }
